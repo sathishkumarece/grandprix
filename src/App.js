7b555f03c2a6767ace7e85b3698db35d5783bd74
sathishkumar NATARAJ <32127136+sathishkumarece@users.noreply.github.com>
2021-01-04 19:11:10 +0530

    Feat: Update on screen lookup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import schoolPamplet from './schoolPamplet.jpg';
-import schoolBuilding from './schoolBuilding.jpg';
-import './App.css';
-import {Row, Col} from 'react-bootstrap'
+import schoolPamphlet from './schoolPamplet.jpg'
+import schoolBuilding from './schoolBuilding.jpg'
+import './App.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,26 +20,45 @@ function App() {
           Learn React
         </a>
       </header>  */}
-      {/* <Container> */}
-        <Row>
-          <Col md={6} sm={12}>
-            <Row>
-              <h1>GrandPrix Global School</h1>
-            </Row>
-            <Row>
-            <img src={schoolBuilding} className="img-responsive" alt="school" />
-            </Row>
-            <Row>
-              <h5>GrandPrix Global School</h5>
-            </Row>
-          </Col>
-          <Col md={6} sm={12}>
-            <img src={schoolPamplet} className="img-responsive" alt="school" />
-          </Col>
-        </Row>
-      {/* </Container> */}
+      <Row>
+        <Col md={6} sm={12}>
+          <Container>
+            <div className='information-container'>
+            <div className='logo-container'>
+              <Row>
+                <h2>GrandPrix </h2>
+              </Row>
+              <Row>
+                <h4>Global School</h4>
+              </Row>
+              </div>
+              <div className='under-construction'>
+                <div className='text-construction'>Site under construction...</div>
+              <Row>
+                <img
+                  src={schoolBuilding}
+                  className='building-img'
+                  alt='school'
+                />
+              </Row>
+              </div>
+              <div className='logo-container'>
+              <Row>
+                <h5>Please refer to the pamphlet for more information</h5>
+              </Row>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col md={6} sm={12}>
+          <div className='pamphlet-container'>
+            <img src={schoolPamphlet} className='pamphlet-img' alt='school' />
+            <div className='pamphlet-construction'></div>
+          </div>
+        </Col>
+      </Row>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
